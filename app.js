beea6f8e2f54d4df01a787becbfde7c265390bb2
Simonni <szkiflay@gmal.com>
2018-11-11 20:00:52 -0500

    adding css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let elList = document.getElementById('studForm')
+let eltable = document.getElementById('studTable')
 let elform = document.getElementById('devForm')
 
 let nameOfStudents = []
@@ -9,51 +9,51 @@ let student = function(name, nameOfSchool, progLang){
   this.progLang = progLang
 }
 
-// if(localStorage.parsenameOfStudents){
-//   let studentStorage = localStorage.getItem('parsenameOfStudents')
-//   nameOfStudents =JSON.parse(studentStorage)
-// } else {}
-
-let elHeader = document.createElement('h4')
-elList.appendChild(elHeader)
-let elTh = document.createElement('h4')
+let elHeader = document.createElement('tr')
+eltable.appendChild(elHeader)
+let elTh = document.createElement('th')
 elHeader.appendChild(elTh)
 elTh.innerText = 'Name of Student'
-let elThschool = document.createElement('h4')
+let elThschool = document.createElement('th')
 elHeader.appendChild(elThschool)
 elThschool.innerText = 'Name of School'
-let elThprogLang = document.createElement('h4')
+let elThprogLang = document.createElement('th')
 elHeader.appendChild(elThprogLang)
 elThprogLang.innerText = 'Programing Language'
 
+// localStorage 
+if(localStorage.parsestudentsArray){
+  let studentStorage = localStorage.getItem('studentsArray')
+  nameOfStudents =JSON.parse(studentStorage)
+}else {
 let student1 = new student('simon', 'code-partners', 'javascript')
 let student2 = new student('Hannah', 'code-partners', 'java')
 let student3 = new student('kesete', 'code-partners', 'javascript')
 
-// creating LocalStorage 
+//nameOfStudents.push(student1,student2,student3)
 
+student.prototype.newNameOfStudent = function(){
+  for(let i=0; i<nameOfStudents.length; i++){
+    let listName = document.createElement('ul')
+    elTh.appendChild(listName)
+    let listschool = document.createElement('ul')
+    elThschool.appendChild(listschool)
+    let listLang = document.createElement('ul')
+    elThprogLang.appendChild(listLang)
 
-//nameOfStudents.push(student1,student2,student3)  
-
-  student.prototype.newNameOfStudent = function(){
-    for(let i=0; i<nameOfStudents.length; i++){
-      let listName = document.createElement('ul')
-      elTh.appendChild(listName)
-      let listschool = document.createElement('ul')
-      elThschool.appendChild(listschool)
-      let listLang = document.createElement('ul')
-      elThprogLang.appendChild(listLang)
-
-      listName.innerText=nameOfStudents[i].name
-      listschool.innerText = nameOfStudents[i].nameOfSchool
-      listLang.innerText = nameOfStudents[i].progLang
-    }
+    listName.innerText=nameOfStudents[i].name
+    listschool.innerText = nameOfStudents[i].nameOfSchool
+    listLang.innerText = nameOfStudents[i].progLang
   }
+}
 
-  for (let i=0; i<nameOfStudents.length; i++){
-    nameOfStudents[i].newNameOfStudent()
-  }
 
+// let total = function(){
+
+for(let i=0; i<nameOfStudents.length; i++){
+  nameOfStudents[i].newNameOfStudent() 
+}
+}
 //Accessing our inputs on our form in html via dot.notation
 let elstudent = elform.name
 let elschool = elform.nameOfSchool
@@ -62,10 +62,10 @@ let ellanguage = elform.progLang
 // Event Listner to our constructor function
 elform.addEventListener('submit', function(event){
   event.preventDefault()
-  localStorage.setItem('parsenameOfStudents', JSON.stringify(nameOfStudents)) 
-//eltable.removeChild(eltable.lastChild)
-  let newStudent = new student(elstudent.value,elschool.value, ellanguage.value)   
+  localStorage.setItem('studentsArray', JSON.stringify(nameOfStudents))
+  // eltable.removeChild(eltable.lastChild)
+  let newStudent = new student(elstudent.value,elschool.value, ellanguage.value)
   nameOfStudents.push(newStudent)
   newStudent.newNameOfStudent()
-
+ 
 })
