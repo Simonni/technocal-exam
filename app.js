ec0b7c85a5c569ccfee533921898ab9770456581
Simonni <szkiflay@gmal.com>
2018-11-11 20:08:46 -0500

    remove red lines

--- app.js
+++ app.js
@@ -26,33 +26,33 @@ if(localStorage.parsestudentsArray){
   let studentStorage = localStorage.getItem('studentsArray')
   nameOfStudents =JSON.parse(studentStorage)
 }else {
-let student1 = new student('simon', 'code-partners', 'javascript')
-let student2 = new student('Hannah', 'code-partners', 'java')
-let student3 = new student('kesete', 'code-partners', 'javascript')
+  let student1 = new student('simon', 'code-partners', 'javascript')
+  let student2 = new student('Hannah', 'code-partners', 'java')
+  let student3 = new student('kesete', 'code-partners', 'javascript')
 
 //nameOfStudents.push(student1,student2,student3)
 
-student.prototype.newNameOfStudent = function(){
-  for(let i=0; i<nameOfStudents.length; i++){
-    let listName = document.createElement('ul')
-    elTh.appendChild(listName)
-    let listschool = document.createElement('ul')
-    elThschool.appendChild(listschool)
-    let listLang = document.createElement('ul')
-    elThprogLang.appendChild(listLang)
+  student.prototype.newNameOfStudent = function(){
+    for(let i=0; i<nameOfStudents.length; i++){
+      let listName = document.createElement('ul')
+      elTh.appendChild(listName)
+      let listschool = document.createElement('ul')
+      elThschool.appendChild(listschool)
+      let listLang = document.createElement('ul')
+      elThprogLang.appendChild(listLang)
 
-    listName.innerText=nameOfStudents[i].name
-    listschool.innerText = nameOfStudents[i].nameOfSchool
-    listLang.innerText = nameOfStudents[i].progLang
+      listName.innerText=nameOfStudents[i].name
+      listschool.innerText = nameOfStudents[i].nameOfSchool
+      listLang.innerText = nameOfStudents[i].progLang
+    }
   }
-}
 
 
 // let total = function(){
 
-for(let i=0; i<nameOfStudents.length; i++){
-  nameOfStudents[i].newNameOfStudent() 
-}
+  for(let i=0; i<nameOfStudents.length; i++){
+    nameOfStudents[i].newNameOfStudent() 
+  }
 }
 //Accessing our inputs on our form in html via dot.notation
 let elstudent = elform.name
